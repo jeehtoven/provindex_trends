a96e8f5b6f38b9cb8d1af1fffb05627e2a1911ea
Jason Banks <me@jeehtoven.com>
2015-05-02 12:19:29 -0400

    Initial Load
    Version 1

---
+++ car_sales_news/index.php
@@ -0,0 +1,13 @@
+<html>
+<title>Provindex Tweets</title>
+<body>
+	<br>
+	<br>
+	<center><h2>Tweets About Agriculture</h2><br><br><a class="twitter-timeline" href="https://twitter.com/search?q=agriculture" data-widget-id="593445376029532161">Tweets about agriculture</a>
+	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></center>
+	<br>
+	<br>
+	<center><h2>Tweets About Infrastructure</h2><br><br><a class="twitter-timeline" href="https://twitter.com/search?q=infrastructure" data-widget-id="593448619572916224">Tweets about infrastructure</a>
+	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></center>
+</body>
+</html>
